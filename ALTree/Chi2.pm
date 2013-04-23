56ad04e8b66e1def2adc08e9f3240732c86d5b68
Claire Bardel <Claire.Bardel@univ-lyon1.fr>
2013-04-23 22:46:38 +0200

    No RCS version anymore, using 1.00 version by default

--- ALTree/Chi2.pm
+++ ALTree/Chi2.pm
@@ -8,7 +8,7 @@ BEGIN {
     our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
     
     # set the version for version checking
-    #$VERSION     = 1.00;
+    $VERSION     = 1.00;
     # if using RCS/CVS, this may be preferred
     #$VERSION = do { my @r = (q$Revision$ =~ /\d+/g); 
     #		    sprintf "%d."."%02d" x $#r, @r }; # must be all one line, for MakeMaker
